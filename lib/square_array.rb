c81f01a7dca9903574540cf897f750beb27a07e1
Iona <ionabrabender@gmail.com>
2020-04-25 22:13:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,7 @@ def square_array(array)
   array.length.times do |index|
     sqr_num = (array[index] + 1) * (array[index] + 1)
     new_array.push(sqr_num)
-    binding.pry
   end
   new_array
+  binding.pry
 end
